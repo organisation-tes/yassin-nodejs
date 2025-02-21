563331aee9f54e853f63aaf1f411f12da30ca5df
cha7pala5 <plsoso46@gmail.com>
2025-02-21 10:08:18 +0100

    first commit

--- src/main/java/com/example/aa/HelloServlet.java
+++ src/main/java/com/example/aa/HelloServlet.java
@@ -18,7 +18,7 @@ public class HelloServlet extends HttpServlet {
 
         // Hello
         PrintWriter out = response.getWriter();
-        out.println("<html>zzzzssdddffggggfdfddddaa<body>");
+        out.println("<html>zzzzssdddffsdsdggggfdfddddaa<body>");
         out.println("<h1>" + message + "</h1>");
         out.println("</body></html>");
     }
